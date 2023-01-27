c9eab405ae185ca86825cae8c662c212422f4907
Alexander Jacobs <123145468+jwrk9000@users.noreply.github.com>
2023-01-27 01:07:34 -0500

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,23 +1,3 @@
-#goal is to create a library that will aid in nlp
-
-#``````````````
-
-#this library will create a way for the program to memorize relevant things about the user
-#based on input and create a sense of friendship
-
-#``````````````
-
-#input then send to box
-#how to create memory
-#long term or short term memory
-#input must then be understood as it grows and then correct response
-#response will come from somewhere
-#input will trigger response and response will be related to input, friendly relationship
-#will listen and respond
-#will listen and learn about user from inputs and respond and become their friend
-
-
-
 
 
 import nltk
@@ -30,27 +10,96 @@ from nltk import pos_tag
 from itertools import chain
 from collections import Counter
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from collections import Counter
 
 
-#input
 
-input_array = []
 
-for i in range(10):
-    input_array.append(input("Enter a value: "))
+
+
+
+
+
+
+
+
+
+
+#input      
+
+
+def moodJudgement():
+
+
+
+
+
+
+
+    input_array = []
+    very_common_words = []
+    positive_sentiments = []
+    negative_sentiments = []
+
+    for i in range(1):
+        input_array.append(input("tell me about your day:       "))
+
     
-tokens = [word_tokenize(i) for i in input_array]
-tokens2 = [sent_tokenize(i) for i in input_array]
-flattened_tokens = list(chain.from_iterable(tokens))
-tagged_tokens = pos_tag(flattened_tokens)
-tag_count = Counter([tag for token, tag in tagged_tokens])
-print(tag_count) #count of pos tags and which occur most frequently
+    tokens = [word_tokenize(i) for i in input_array]#tokens
+    more_tokens = [sent_tokenize(i) for i in input_array] #seperate variable for tokens
+    flattened_tokens = list(chain.from_iterable(tokens))#flattened tokens
+    tagged_tokens = pos_tag(flattened_tokens)#pos tagged tokens
+    tag_count = Counter([tag for token, tag in tagged_tokens])#count for pos tags
+    print(tag_count) #count of which occur most frequently
 
-text = " ".join(flattened_tokens)
-sia = SentimentIntensityAnalyzer()
-sentiment = sia.polarity_scores(text)
-print(sentiment) #judges sentiment vaguely
+    
+    text = " ".join(flattened_tokens)#flattened tokens turned back into text
+    sia = SentimentIntensityAnalyzer()
+    sentiment = sia.polarity_scores(text)
+    print(sentiment) #judges sentiment vaguely, the 1 is positive, and -1 is negative
 
 
-#goal is to create a friendship, regardless of if the input is good or bad 
-#goal is to now use the their mood and tempo to respond better.
+    #asume that 'flattened_tokens' is a list of tokenized words
+    word_count = Counter(flattened_tokens)
+    #print the 10 most common words and their frequency
+    for word, count in word_count.most_common(10):
+        print(f'{word}: {count}')
+
+    
+    # Iterate over the word count dictionary
+    for word, count in word_count.most_common(5):
+    # Add the word to the very_common_words list
+        very_common_words.append(word)
+    print(very_common_words)
+
+    
+    if sentiment['compound'] > 0:
+        positive_sentiments.append("good mood")
+    print(positive_sentiments)
+
+
+    if sentiment['compound'] < 0:
+        negative_sentiments.append("bad mood")
+    print(negative_sentiments)
+
+
+    #try to identify sadness
+
+    #try to indentify nervousnesss
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+moodJudgement()
+#improve what we have ?
